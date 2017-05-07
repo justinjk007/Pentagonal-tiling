a3c3135a4c87691678d6451661e4b3724d18f93b
Justin K -Linux <justinjoseph0007@gmail.com>
2017-05-06 21:31:41 -0400

    Added back accidentally deleted files

---
+++ src/CalculateGap.hpp
@@ -0,0 +1,7 @@
+#ifndef CALCULATEGAP_H
+#define CALCULATEGAP_H
+
+double getGap(Tile);
+bool validateTile(Tile); 
+
+#endif /* CALCULATEGAP_H */
---
+++ src/Tile.hpp
@@ -0,0 +1,16 @@
+#ifndef TILE_H
+#define TILE_H
+
+struct Tile{
+  double side_a;
+  double side_b;
+  double side_c;
+  double side_d;
+  double side_e;
+  double angle_a;
+  double angle_b;
+  double angle_c;
+  double angle_d;
+};
+
+#endif /* TILE_H */
