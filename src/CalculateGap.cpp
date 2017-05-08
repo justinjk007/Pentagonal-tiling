17a8b9dd7e37dc2632f835e9ed1fd83eb3d9e850
Justin K -W10 <justinjoseph0007@gmail.com>
2017-05-08 18:27:38 -0400

    Re-implementation of the pentagonal visualization

--- src/CalculateGap.cpp
+++ src/CalculateGap.cpp
@@ -22,7 +22,7 @@ bool validateTile(Tile tile){
    */
   double max_side = getMaxSide(tile);
   if(tile.angle_a+tile.angle_b+tile.angle_c+tile.angle_d < 540)
-    if(tile.side_a+tile.side_b+tile.side_c+tile.side_d+tile.side_e > (2*max_side))
+    if(tile.side_ab+tile.side_bc+tile.side_cd+tile.side_de+tile.side_ea > (2*max_side))
       // 2 x Biggest side becauase we don't know which one is the
       // biggest side now and we should count that twice on the right
       // side because we are taking the sum of all sides instead of
@@ -39,14 +39,14 @@ double getMaxSide(Tile tile){
    *This method returns the biggest side of the tile.
    *Pretty bad way to do it I think
    */
-  double max_side = tile.side_a;
-  if (tile.side_b > max_side)
-    max_side = tile.side_b;
-  if (tile.side_c > max_side)
-    max_side = tile.side_c;
-  if (tile.side_d > max_side)
-    max_side = tile.side_d;
-  if (tile.side_e > max_side)
-    max_side = tile.side_e;
+  double max_side = tile.side_ab;
+  if (tile.side_bc > max_side)
+    max_side = tile.side_bc;
+  if (tile.side_cd > max_side)
+    max_side = tile.side_cd;
+  if (tile.side_de > max_side)
+    max_side = tile.side_de;
+  if (tile.side_ea > max_side)
+    max_side = tile.side_ea;
   return max_side;
 }
--- src/Tile.hpp
+++ src/Tile.hpp
@@ -2,11 +2,11 @@
 #define TILE_H
 
 struct Tile{
-  double side_a;
-  double side_b;
-  double side_c;
-  double side_d;
-  double side_e;
+  double side_ab;
+  double side_bc;
+  double side_cd;
+  double side_de;
+  double side_ea;
   double angle_a;
   double angle_b;
   double angle_c;
