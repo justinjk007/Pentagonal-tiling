823b171e41d32b641c246aab5c60d288283c9210
Justin K -W10 <justinjoseph0007@gmail.com>
2017-05-14 22:49:23 -0400

    Wrote test for addTile

--- src/test.cpp
+++ src/test.cpp
@@ -42,6 +42,20 @@ BOOST_AUTO_TEST_CASE(primitive_tile_test){
   BOOST_CHECK_EQUAL(one.checkSide(tile.side_bc), false);
 }
 
+BOOST_AUTO_TEST_CASE(primitive_tile_add_tile){
+  Tile tile1={10,7,21,20,30,54,54,45,45};
+  Tile tile2={1,17,21,30,40,54,54,45,45};
+  Tile tile3={1,17,23,30,40,54,54,45,45};
+  PrimitiveTile newSample(tile1);
+  Link link1(&tile1.side_cd,&tile2.side_cd);
+  Link link2(&tile1.side_cd,&tile3.side_cd);
+  bool answer1 = newSample.addTile(tile2, link1);
+  bool answer2 = newSample.addTile(tile3, link2);
+  BOOST_CHECK_EQUAL(answer1,true);
+  BOOST_CHECK_EQUAL(answer2,false);
+  BOOST_CHECK_EQUAL(newSample.size, 2);
+}
+
 BOOST_AUTO_TEST_CASE(get_gap_the_main_funtion){
   Tile tile ={3.22, 3.10, 2.20, 2.63, 3.43, 118, 39, 330, 66};
   BOOST_CHECK_EQUAL(getGap(tile),0);
