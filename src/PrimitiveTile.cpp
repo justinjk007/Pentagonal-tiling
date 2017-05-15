c43276acaf39748b62fecf1645c122b97877cd07
Justin K -W10 <justinjoseph0007@gmail.com>
2017-05-14 23:39:38 -0400

    Deleted unwanted stuff like the macros

--- src/PrimitiveTile.cpp
+++ src/PrimitiveTile.cpp
@@ -1,9 +1,6 @@
 #include "PrimitiveTile.hpp"
 #include <iostream>
 
-#define SHOW(a) std::cout << #a << " : " << (a) << std::endl
-// Macro for variable name;
-
 using namespace std;
 
 bool PrimitiveTile::checkSide(Side side){
@@ -27,10 +24,6 @@ void PrimitiveTile::print(){
   cout << "\n";
   for (int i = 1; i < this->size; i++)
     {
-      // SHOW(this->links[i]);
-      cout << links[i] << endl;
-      cout << links[i]->from << endl;
-      cout << links[i]->to->value << endl;
       cout << this->links[i]->from->value <<"  "<<links[i]->to->value<<"\n";
     }
 
