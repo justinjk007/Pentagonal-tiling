57e54c4937f2059abf9d3db2c9b80f3d74c871b4
Justin K -W10 <justinjoseph0007@gmail.com>
2017-05-30 12:22:37 -0400

    Cmake and boost intergration works on all platforms now

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "Tile.hpp"
 #include "CalculateGap.hpp"
 #include "PrimitiveTile.hpp"
+#include <boost/test/included/unit_test.hpp>
 #include <boost/test/unit_test.hpp>
 #include <iostream>
 
