8fa2e019c485fade088e6ac85c5713b8472c1d66
Justin K -Linux <justinjoseph0007@gmail.com>
2017-07-16 23:44:57 -0400

    Added libs and removed unwanted escape sequence

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include "CalculateGap.hpp"
 #include "Tile.hpp"
-#include <iostream>
-#include <string>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 using namespace std;
 
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
                          atof(argv[11])};
         calculateWithDimensions(pentagon);
       } else
-	errorInArguments();
+        errorInArguments();
     } else if (strcmp(argv[1], "-f") == 0)
       readFromFile();
     else
@@ -54,15 +54,15 @@ void helper() {
   printf("Pentagonal Tiling %s\n", VERSION);
   printf("Copyright (c) %s, %s.  All rights reserved.\n", YEAR, AUTHOR);
   printf(
-	 "\nTo use the program call the executable and pass either 9 or 10 \n"
-	 "arguments, which are the dimensions of the pentagon you're trying to \n"
-	 "get the gap from. \n");
+         "\nTo use the program call the executable and pass either 9 or 10 \n"
+         "arguments, which are the dimensions of the pentagon you're trying to \n"
+         "get the gap from. \n");
   printf("The first five arguments must be dimensions of the sides\n");
   printf("The next four of five are measurement of the angles in degrees\n");
   printf("To pass arguments this way you should also use the flag \"-d\" or "
          "\"-dimensions\". \n");
   printf("For example, this is a correct way to invoke the program\n");
-  printf("\.\/%s\.exe -d 32.72 73.59 78.67 49.06 57.68 122.03 115.73 84.19 "
+  printf("./%s.exe -d 32.72 73.59 78.67 49.06 57.68 122.03 115.73 84.19 "
          "95.81 122.23\n",
          PROG_NAME);
   printf("\nYou can also pass arguments in a file with many calculations to "
@@ -72,7 +72,7 @@ void helper() {
   printf("Or just the name of the file if its in the same directory as the "
          "executable \n");
   printf("For example, this is another correct way to invoke the program\n");
-  printf("\.\/%s\.exe -f data.csv\n", PROG_NAME);
+  printf("./%s.exe -f data.csv\n", PROG_NAME);
   printf("Only .csv files are parsable now\n");
   printf("\nPassing dimensions returns the gap here\n");
   printf("Passing a file writes the answer into the file called result.dat\n");
@@ -80,7 +80,7 @@ void helper() {
 
 void calculateWithDimensions(Tile tile) {
   double minimum_gap = getGap(tile);
-  printf("\n Minimum gap calculated is : %f",minimum_gap);
+  printf("\n Minimum gap calculated is : %f", minimum_gap);
 }
 
 void readFromFile() {}
