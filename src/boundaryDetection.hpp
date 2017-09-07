015477adb0a9aea80a54c42bc631774eb6feca6a
Justin K -W10 <justinjoseph0007@gmail.com>
2017-09-07 18:24:07 -0400

    Removed Alphashape / Concave hull stuff
    Becuase I don't use them anymore because upon testing it was found
    that when the polygons are concave steeply the alphashape produced is
    not the correct shape of the biggest polygon obtainable. My new
    function for boundary detection replaces the use of this.
    
    RIP

--- src/boundaryDetection.hpp
+++ src/boundaryDetection.hpp
@@ -1,8 +1,6 @@
-#ifndef CONCAVEHULL_HPP
-#define CONCAVEHULL_HPP
+#ifndef BOUNDARYDETECTION_HPP
+#define BOUNDARYDETECTION_HPP
 
-#include <CGAL/Alpha_shape_2.h>
-#include <CGAL/Delaunay_triangulation_2.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/algorithm.h>
 #include <CGAL/Simple_cartesian.h>
@@ -22,25 +20,10 @@ typedef K::Segment_2 Segment;
 typedef K::Iso_rectangle_2 Rect;
 typedef CGAL::Polygon_2<K> Polygon_2;
 
-typedef CGAL::Alpha_shape_vertex_base_2<K> Vb;
-typedef CGAL::Alpha_shape_face_base_2<K> Fb;
-typedef CGAL::Triangulation_data_structure_2<Vb, Fb> Tds;
-typedef CGAL::Delaunay_triangulation_2<K, Tds> Triangulation_2;
-
-typedef CGAL::Alpha_shape_2<Triangulation_2> Alpha_shape_2;
-typedef Alpha_shape_2::Alpha_shape_edges_iterator Alpha_shape_edges_iterator;
-
-
-template <class OutputIterator>
-void alpha_edges(const Alpha_shape_2&, OutputIterator);
-template <class OutputIterator>
-bool file_input(OutputIterator);
-std::list<Segment> getConcaveHull(std::list<Point_2>);
-
 Rect getBoundingBox(std::list<Point_2>);
 std::list<Segment> removeInnerLines(std::list<Line>);
 std::list<Segment> getSegments(std::list<Line>); // TODO: Remove this
 bool doIntersect(Segment, std::list<Segment>, Segment);
 Polygon_2 getPolygon(std::list<Point_2>);
 
-#endif /* CONCAVEHULL_HPP */
+#endif /* BOUNDARYDETECTION_HPP */
